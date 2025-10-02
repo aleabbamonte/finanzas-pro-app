b2fc6ae7219fe451b3fcf194ee5c95de016a6cbe
aleabbamonte <ale.abbamonte@gmail.com>
2025-10-02 15:23:46 -0300

    Version 2.0

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+javascriptconst CACHE_NAME = 'finanzas-pro-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalación del service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activación y limpieza de cachés antiguos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar peticiones y servir desde caché
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
